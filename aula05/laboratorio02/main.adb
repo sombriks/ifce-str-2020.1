9578cbddba86b93a46dc03d50e3a1647e2f97afd
Leonardo Silveira <sombriks@gmail.com>
2020-03-11 16:46:06 -0300

    fazendo com entry/accept

--- aula05/laboratorio02/main.adb
+++ aula05/laboratorio02/main.adb
@@ -5,15 +5,52 @@ with MyArray;
 with Trabalho;
 
 procedure main is
+   B : Boolean;
+   I1 : Integer := 0;
+   I2 : Integer := 10;
+   I3 : Integer := 20;
    Arr : MyArray.ArrArr := ('?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?');
     -- declarando as tasks
-  --  task T1;
-  --  task T2;
-  --  task T3;
-   B : Boolean;
-   I : Integer := 0;
+   task T1 is
+    entry Preenche;
+    entry Imprime;
+   end T1;
+   task T2 is
+    entry Preenche;
+    entry Imprime;
+   end T2;
+   task T3 is 
+    entry Preenche;
+    entry Imprime;
+   end T3;
+  -- body das tasks
+  task body T1 is
+  begin
+    accept Preenche;
+      B := Trabalho.Preenche(Arr,I1,9,'a');
+      T2.Preenche;
+    accept Imprime;
+      B := Trabalho.Imprime(Arr,0,9);
+      T2.Imprime;
+
+  end T1;
+  task body T2 is
+  begin
+    accept Preenche;
+      B := Trabalho.Preenche(Arr,I2,19,'b');
+      T3.Preenche;
+    accept Imprime;
+      B := Trabalho.Imprime(Arr,10,19);
+      T3.Imprime;
+  end T2;
+  task body T3 is
+  begin
+    accept Preenche;
+      B := Trabalho.Preenche(Arr,I3,29,'c');
+      T1.Imprime;
+    accept Imprime;
+      B := Trabalho.Imprime(Arr,20,29);
+  end T3;
 begin
-  B := Trabalho.Imprime(Arr,0,29);
-  B := Trabalho.Preenche(Arr,I,29,'a');
-  B := Trabalho.Imprime(Arr,0,29);
+  T1.Preenche;
 end main;
